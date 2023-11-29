9ab4923ec1503481cde3860d2c7166fe430aa30d
imadelbouazzaoui <imad.elbouazzaoui.ee@gmail.com>
2023-11-29 14:04:55 +0100

    Creating Event data class

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+enum class Daypart{
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
+
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
+
+
+fun main(){
+
+}
